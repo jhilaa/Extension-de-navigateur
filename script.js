d13ce22a2db8e7458fc51fc35af19379d1e31bf9
Julien Hilaire <julien.hilaire@imelmi.net>
2021-09-24 18:32:20 +0200

    popup resizable, contenu éditable (mais dans le code html donc pas top), bouton pour la mise en forme, export en html pour conserver la mise en forme

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
+let divContainer = document.createElement("div");
+
+divContainer.setAttribute("id", "divContainer");
+divContainer.style.position = "fixed";
+divContainer.style.top = "2px";
+divContainer.style.left = "2px";
+divContainer.style.width = "60vh";
+divContainer.style.height = "50vh";
+divContainer.style.backgroundColor = "#e7e9eb";
+divContainer.style.border = "1px darkgrey solid";
+divContainer.style.zIndex = 1000;
+divContainer.style.resize = "both";
+divContainer.style.overflow = "auto";
+document.body.appendChild(divContainer);
+
+console.log(divContainer);
+
 let divNote = document.createElement("div");
+divNote.style.width = "95%";
+divNote.style.height = "75%";
+divNote.style.fontFamily = "Arial, Helvetica";
+divNote.style.fontSize = "12px";
+divNote.style.margin = "5px";
+divContainer.style.backgroundColor = "white";
+divNote.style.border = "1px solid green";
+divNote.style.position = "relative";
+divNote.style.overflow = "scroll";
 
-divNote.setAttribute("id", "divNote");
-divNote.style.position = "fixed";
-divNote.style.top = "2px";
-divNote.style.left = "2px";
-divNote.style.width = "50vh";
-divNote.style.height = "50vh";
-divNote.style.backgroundColor = "#e7e9eb";
-divNote.style.border = "1px darkgrey solid";
-divNote.style.zIndex = 1000;
-document.body.appendChild(divNote);
+divContainer.appendChild(divNote);
 
-console.log(divNote);
+let divTextArea = document.createElement("textarea");
+divTextArea.style.resize = "none";
+divTextArea.style.width = "95%";
+divTextArea.style.height = "75%";
+divTextArea.style.fontFamily = "Arial, Helvetica";
+divNote.style.fontSize = "12px";
+divTextArea.style.margin = "5px";
+divTextArea.style.border = "1px solid red";
+divTextArea.style.position = "relative";
+divTextArea.style.display = "none";
 
-let divTextArray = document.createElement("textarea");
-divTextArray.style.resize = "none";
-divTextArray.style.width = "95%";
-divTextArray.style.height = "75%";
-divTextArray.style.fontFamily = "Arial, Helvetica";
-divTextArray.style.margin = "5px";
-divTextArray.style.border = "1px solid grey";
-divTextArray.style.position = "relative";
+divContainer.appendChild(divTextArea);
 
-divNote.appendChild(divTextArray);
-
+/*
 let btnSize = document.createElement("button");
 btnSize.textContent = "Resize";
 btnSize.classList.add("btnResize");
@@ -32,15 +50,121 @@ btnSize.style.width = "50px";
 btnSize.style.position = "relative";
 btnSize.style.backgroundColor = "#059862";
 //---
-divNote.appendChild(btnSize);
+divContainer.appendChild(btnSize);
 //---
 btnSize.onclick = function () {
   if (btnSize.classList.contains("btnResize")) {
-    document.getElementById("divNote").style.height = "10px";
+    document.getElementById("divContainer").style.height = "10px";
     btnSize.classList.replace("btnResize", "btnExpand");
   } else {
-    document.getElementById("divNote").style.height = "50vh";
+    document.getElementById("divContainer").style.height = "50vh";
     btnSize.classList.replace("btnExpand", "btnResize");
+  }
+};
+*/
+
+//=====================
+let btnPaste = document.createElement("button");
+btnPaste.textContent = "Paste";
+btnPaste.style.height = "40px";
+btnPaste.style.width = "50px";
+btnPaste.style.position = "relative";
+btnPaste.style.backgroundColor = "#5df200";
+btnPaste.style.margin = "2px";
+//---
+divContainer.appendChild(btnPaste);
+
+btnPaste.onclick = function () {
+  let selectedText = window.getSelection().toString();
+  selectedText = selectedText.replace(/(?:\r\n|\r|\n)/g, "<br>");
+  if (divTextArea.value == "") {
+    divTextArea.value = selectedText;
+  } else {
+    divTextArea.value = divTextArea.value + "<br><br>" + selectedText;
+  }
+  divNote.innerHTML = divTextArea.value;
+};
+
+//=====================
+let btnClear = document.createElement("button");
+btnClear.textContent = "Clear";
+btnClear.style.height = "40px";
+btnClear.style.width = "50px";
+btnClear.style.position = "relative";
+btnClear.style.backgroundColor = "#f435c5";
+btnClear.style.margin = "2px";
+//---
+divContainer.appendChild(btnClear);
+
+btnClear.onclick = function () {
+  divTextArea.value = "";
+  divNote.innerHTML = "";
+};
+
+//=====================
+let btnExport = document.createElement("button");
+btnExport.textContent = "Export";
+btnExport.style.height = "40px";
+btnExport.style.width = "50px";
+btnExport.style.position = "relative";
+btnExport.style.backgroundColor = "#277CF1";
+btnExport.style.margin = "2px";
+//---
+divContainer.appendChild(btnExport);
+
+btnExport.onclick = function () {
+  var textFileAsBlob = new Blob([divTextArea.value], {
+    type: "text/plain",
+  });
+  var fileNameToSaveAs = "file.html";
+  var downloadLink = document.createElement("a");
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innerHTML = "Download File";
+
+  // Chrome allows the link to be clicked
+  // without actually adding it to the DOM.
+  downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  downloadLink.click();
+};
+
+//=====================
+let btnBold = document.createElement("button");
+btnBold.textContent = "Bold";
+btnBold.style.height = "40px";
+btnBold.style.width = "50px";
+btnBold.style.position = "relative";
+btnBold.style.backgroundColor = "#d5f200";
+btnBold.style.margin = "2px";
+//---
+divContainer.appendChild(btnBold);
+
+btnBold.onclick = function () {
+  var selection = window.getSelection();
+  var selectionText = selection.toString();
+  const regex = new RegExp(selectionText, "g");
+  divTextArea.value = divTextArea.value.replace(
+    regex,
+    "<span style='background-color:yellow;'>" + selectionText + "</span>"
+  );
+  divNote.innerHTML = divTextArea.value;
+};
+
+//=====================
+let btnEdit = document.createElement("button");
+btnEdit.textContent = "Edit";
+btnEdit.style.height = "40px";
+btnEdit.style.width = "50px";
+btnEdit.style.position = "relative";
+btnEdit.style.backgroundColor = "#2aaCF1";
+btnEdit.style.margin = "2px";
+//---
+divContainer.appendChild(btnEdit);
+
+btnEdit.onclick = function () {
+  if (divTextArea.style.display == "none") {
+    divTextArea.style.display = "block";
+  } else {
+    divTextArea.style.display = "none";
   }
 };
 
@@ -53,54 +177,8 @@ btnSave.style.position = "relative";
 btnSave.style.backgroundColor = "#277CF1";
 btnSave.style.margin = "2px";
 //---
-divNote.appendChild(btnSave);
-
-//=====================
-let btnPaste = document.createElement("button");
-btnPaste.textContent = "Paste";
-btnPaste.style.height = "40px";
-btnPaste.style.width = "50px";
-btnPaste.style.position = "relative";
-btnPaste.style.backgroundColor = "#d5f200";
-btnPaste.style.margin = "2px";
-//---
-divNote.appendChild(btnPaste);
-
-btnPaste.onclick = function () {
-  if (divTextArray.value == "") {
-    divTextArray.value = window.getSelection().toString();
-  } else {
-    divTextArray.value =
-      divTextArray.value + "\n" + window.getSelection().toString();
-  }
-};
-
-//=====================
-let btnClear = document.createElement("button");
-btnClear.textContent = "Clear";
-btnClear.style.height = "40px";
-btnClear.style.width = "50px";
-btnClear.style.position = "relative";
-btnClear.style.backgroundColor = "#f435c5";
-btnClear.style.margin = "2px";
-//---
-divNote.appendChild(btnClear);
-
-btnClear.onclick = function () {
-  divTextArray.value = "";
-};
+divContainer.appendChild(btnSave);
 
 btnSave.onclick = function () {
-  var textFileAsBlob = new Blob([divTextArray.value], {
-    type: "text/plain",
-  });
-  var fileNameToSaveAs = "file.txt";
-  var downloadLink = document.createElement("a");
-  downloadLink.download = fileNameToSaveAs;
-  downloadLink.innerHTML = "Download File";
-
-  // Chrome allows the link to be clicked
-  // without actually adding it to the DOM.
-  downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-  downloadLink.click();
+  divNote.innerHTML = divTextArea.value;
 };
